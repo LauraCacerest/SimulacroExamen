c4824001a245568048f78352f241dc26a72c098d
lauracacerest-simulacroexamen-rfkubeef <lauracacerest-simulacroexamen-rfkubeef@local>
2018-05-24 21:33:53 +0000

    ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,8 @@
 #Escriba el codigo necesario para que al ejecutar
+import numpy as np
+
+for i in range(10,-1,-1):
+    print i
+
+
 #python ejercicio1.py se impriman los enteros del 10 al 1 en cuenta regresiva.
